c21ef2557a42743d96172a8f7fe6edaee97312bd
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-29 11:28:38 +0700

    menambahkan properti dan method

--- produk.php
+++ produk.php
@@ -4,7 +4,23 @@
 // Game
 class Produk
 {
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = "0";
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
 $produk1 = new Produk();
-$produk2 = new Produk();
+$produk1->judul = "Oregairu";
+$produk1->penulis = "Watari Wataru";
+$produk1->penerbit = "Shogakukan";
+$produk1->harga = 28000;
+
+echo "Komik : $produk1->judul, $produk1->penulis";
+echo "<br>";
+echo $produk1->getLabel();
