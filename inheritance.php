69070e9bcfec2ac93e4b5381cf103054440c1c5c
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-29 12:47:49 +0700

    menambahkan inheritance

---
+++ inheritance.php
@@ -0,0 +1,68 @@
+<?php
+// Jualan produk
+// Komik
+// Game
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga,
+        $jmlHalaman,
+        $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfoProduk()
+    {
+        $str =  "{$this->judul} | {$this->getLabel()} ({$this->harga})";
+        return $str;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class Komik extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str =  "Komik : {$this->judul} | {$this->getLabel()} ({$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public function getInfoProduk()
+    {
+        $str =  "Game : {$this->judul} | {$this->getLabel()} ({$this->harga}) ~ {$this->jmlHalaman} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Komik("OreGairu", "Watari Wataru", "Shogakukan", 28000, 100, 0);
+$produk2 = new Game("7DS Grand Cross", "Yo Kokukuji", "Netmarble", 0, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
